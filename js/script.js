80da67a7c3d3ecfbc556960ea80d95940797fbac
Sara <saracorsob@gmail.com>
2020-05-20 16:18:55 +0200

    Aggiunta sezione con if per avere opzione Prezzo non disponibile

--- js/script.js
+++ js/script.js
@@ -2,22 +2,57 @@
 var kmUtente = parseInt(prompt('Quanti chilometri vuole percorrere?'));
 var etaUtente = parseInt(prompt('Quanti anni ha?'));
 
-// Calcolo prezzo totale km
-var prezzoTot = kmUtente * 0.21;
+if ( isNaN(kmUtente) || isNaN(etaUtente) ) {
+  alert('Errore: Il numero che hai digitato non è valido.')
+  var prezzoTot = 'Prezzo non disponibile';
+  document.getElementById('prezzo').innerHTML = prezzoTot;
+  console.log(prezzoTot);
 
-// Calcolo sconti
-var scontoMin = ( ( prezzoTot * 20 ) / 100 );
-var scontoOver = ( ( prezzoTot * 40 ) / 100 );
-
-// Calcolo prezzi scontati
-var prezzoFinMin = prezzoTot - scontoMin;
-var prezzoFinOver = prezzoTot - scontoOver;
-
-// Condizioni
-if ( etaUtente < 18 ) {
-  document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinMin + ' €';
-} else if ( etaUtente > 65 ) {
-  document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinOver + ' €';
 } else {
-  document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoTot + ' €';
+  // Calcolo prezzo totale km
+  var prezzoTot = kmUtente * 0.21;
+  console.log(prezzoTot);
+
+  // Calcolo sconti
+  var scontoMin = ( ( prezzoTot * 20 ) / 100 );
+  var scontoOver = ( ( prezzoTot * 40 ) / 100 );
+  console.log(scontoMin);
+  console.log(scontoOver);
+
+  // Calcolo prezzi scontati
+  var prezzoFinMin = prezzoTot - scontoMin;
+  var prezzoFinOver = prezzoTot - scontoOver;
+  console.log(prezzoFinMin);
+  console.log(prezzoFinOver);
+
+  // Condizioni
+  if ( etaUtente < 18 ) {
+    document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinMin + ' €';
+  } else if ( etaUtente > 65 ) {
+    document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinOver + ' €';
+  } else {
+    document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoTot + ' €';
+  }
+
 }
+
+
+// // Calcolo prezzo totale km
+// var prezzoTot = kmUtente * 0.21;
+//
+// // Calcolo sconti
+// var scontoMin = ( ( prezzoTot * 20 ) / 100 );
+// var scontoOver = ( ( prezzoTot * 40 ) / 100 );
+//
+// // Calcolo prezzi scontati
+// var prezzoFinMin = prezzoTot - scontoMin;
+// var prezzoFinOver = prezzoTot - scontoOver;
+//
+// // Condizioni
+// if ( etaUtente < 18 ) {
+//   document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinMin + ' €';
+// } else if ( etaUtente > 65 ) {
+//   document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinOver + ' €';
+// } else {
+//   document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoTot + ' €';
+// }
