deffb01eda83dbb9ac6f2ac0bf9a0b9205dc6cc5
Sara <saracorsob@gmail.com>
2020-05-20 15:59:14 +0200

    Correzione prezzi + tolto debug

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 // Richieste all'utente
 var kmUtente = parseInt(prompt('Quanti chilometri vuole percorrere?'));
 var etaUtente = parseInt(prompt('Quanti anni ha?'));
-console.log(kmUtente);
-console.log(etaUtente);
 
 // Calcolo prezzo totale km
 var prezzoTot = kmUtente * 0.21;
-console.log(prezzoTot);
+
+// Calcolo sconti
+var scontoMin = ( ( prezzoTot * 20 ) / 100 );
+var scontoOver = ( ( prezzoTot * 40 ) / 100 );
 
 // Calcolo prezzi scontati
-var scontoMin = ( ( prezzoTot * 20 ) / 100 );
-console.log(scontoMin);
-
-var scontoOver = ( ( prezzoTot * 40 ) / 100 );
-console.log(scontoOver);
-
-// Prezzi scontati finali
 var prezzoFinMin = prezzoTot - scontoMin;
-console.log(prezzoFinMin);
 var prezzoFinOver = prezzoTot - scontoOver;
-console.log(prezzoFinOver);
 
 // Condizioni
 if ( etaUtente < 18 ) {
-  document.getElementById('prezzo').innerHTML = 'Il tuo prezzo è ' + prezzoFinMin + ' €';
+  document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinMin + ' €';
 } else if ( etaUtente > 65 ) {
-  document.getElementById('prezzo').innerHTML = 'Il tuo prezzo è ' + prezzoFinOver + ' €';
+  document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinOver + ' €';
 } else {
-  document.getElementById('prezzo').innerHTML = 'Il tuo prezzo è ' + prezzoTot + ' €';
+  document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoTot + ' €';
 }
