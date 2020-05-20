61ff3144540d1778b795627b24407b39414b2b2c
Sara <saracorsob@gmail.com>
2020-05-20 16:19:53 +0200

    Tolto debug

--- js/script.js
+++ js/script.js
@@ -6,24 +6,18 @@ if ( isNaN(kmUtente) || isNaN(etaUtente) ) {
   alert('Errore: Il numero che hai digitato non è valido.')
   var prezzoTot = 'Prezzo non disponibile';
   document.getElementById('prezzo').innerHTML = prezzoTot;
-  console.log(prezzoTot);
 
 } else {
   // Calcolo prezzo totale km
   var prezzoTot = kmUtente * 0.21;
-  console.log(prezzoTot);
 
   // Calcolo sconti
   var scontoMin = ( ( prezzoTot * 20 ) / 100 );
   var scontoOver = ( ( prezzoTot * 40 ) / 100 );
-  console.log(scontoMin);
-  console.log(scontoOver);
 
   // Calcolo prezzi scontati
   var prezzoFinMin = prezzoTot - scontoMin;
   var prezzoFinOver = prezzoTot - scontoOver;
-  console.log(prezzoFinMin);
-  console.log(prezzoFinOver);
 
   // Condizioni
   if ( etaUtente < 18 ) {
@@ -35,24 +29,3 @@ if ( isNaN(kmUtente) || isNaN(etaUtente) ) {
   }
 
 }
-
-
-// // Calcolo prezzo totale km
-// var prezzoTot = kmUtente * 0.21;
-//
-// // Calcolo sconti
-// var scontoMin = ( ( prezzoTot * 20 ) / 100 );
-// var scontoOver = ( ( prezzoTot * 40 ) / 100 );
-//
-// // Calcolo prezzi scontati
-// var prezzoFinMin = prezzoTot - scontoMin;
-// var prezzoFinOver = prezzoTot - scontoOver;
-//
-// // Condizioni
-// if ( etaUtente < 18 ) {
-//   document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinMin + ' €';
-// } else if ( etaUtente > 65 ) {
-//   document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoFinOver + ' €';
-// } else {
-//   document.getElementById('prezzo').innerHTML = 'Il prezzo finale del tuo biglietto è di ' + prezzoTot + ' €';
-// }
