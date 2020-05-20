24a1f9e1962111b5ee5c7c7ab4b91a2fc3f0dd14
Sara <saracorsob@gmail.com>
2020-05-20 15:32:43 +0200

    Stesura prima parte

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-document.getElementById('prezzo').innerHTML = 'Ciao';
+// Richieste all'utente
+var kmUtente = prompt('Quanti chilometri vuole percorrere?');
+var etaUtente = prompt('Quanti anni ha?');
+console.log(kmUtente);
+console.log(etaUtente);
+
+//Calcolo prezzo totale km
+var prezzoTot = kmUtente * 0.21;
+console.log(prezzoTot);
+
+//Calcolo prezzi scontati
+var
