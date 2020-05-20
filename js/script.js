cf2cd320891edb68da47391eaa6fea63b6e75388
Sara <saracorsob@gmail.com>
2020-05-20 15:52:08 +0200

    Stesura seconda parte + debug

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 // Richieste all'utente
-var kmUtente = prompt('Quanti chilometri vuole percorrere?');
-var etaUtente = prompt('Quanti anni ha?');
+var kmUtente = parseInt(prompt('Quanti chilometri vuole percorrere?'));
+var etaUtente = parseInt(prompt('Quanti anni ha?'));
 console.log(kmUtente);
 console.log(etaUtente);
 
-//Calcolo prezzo totale km
+// Calcolo prezzo totale km
 var prezzoTot = kmUtente * 0.21;
 console.log(prezzoTot);
 
-//Calcolo prezzi scontati
-var
+// Calcolo prezzi scontati
+var scontoMin = ( ( prezzoTot * 20 ) / 100 );
+console.log(scontoMin);
+
+var scontoOver = ( ( prezzoTot * 40 ) / 100 );
+console.log(scontoOver);
+
+// Prezzi scontati finali
+var prezzoFinMin = prezzoTot - scontoMin;
+console.log(prezzoFinMin);
+var prezzoFinOver = prezzoTot - scontoOver;
+console.log(prezzoFinOver);
+
+// Condizioni
+if ( etaUtente < 18 ) {
+  document.getElementById('prezzo').innerHTML = 'Il tuo prezzo è ' + prezzoFinMin + ' €';
+} else if ( etaUtente > 65 ) {
+  document.getElementById('prezzo').innerHTML = 'Il tuo prezzo è ' + prezzoFinOver + ' €';
+} else {
+  document.getElementById('prezzo').innerHTML = 'Il tuo prezzo è ' + prezzoTot + ' €';
+}
